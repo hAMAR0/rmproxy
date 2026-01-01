e25c0a7d9b13fe213b25d6e482009a8b11a8494c
hAMAR0 <marvladmrvlad@gmail.com>
2026-01-01 20:26:33 +0500

    init commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(char *s) {
+	perror(s);
+	exit(1);
+}
+
+int main () {
+}
+
